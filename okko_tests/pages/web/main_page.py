7ce09e9d089224fdb0923c04fc5993bcceaa0e7f
Kirill Ermakov <lvstrng161@gmail.com>
2024-05-24 11:01:28 +0400

    main_page.py __init__ refactor

--- okko_tests/pages/web/main_page.py
+++ okko_tests/pages/web/main_page.py
@@ -5,59 +5,71 @@ from selenium.common import NoSuchElementException
 
 
 class MainPage:
+
+    def __init__(self):
+        self.registration_form = s('a[test-id="nav_sign"]')
+        self.registration_option = s(f'[test-id="sign_"]')
+        self.search_button = s('[test-id="nav_search"]')
+        self.search_input_field = s('[test-id="nav_search_input"]')
+        self.sections = ss('[test-id="rail_tab_item"]')
+        self.page_title = s('[test-id="collection_page_title"]')
+        self.collection_of_elements = ss('[test-id="search_collection_element"]')
+        self.category = s('[test-id="search_collection_element"]')
+        self.film_genre = s('[test-id="meta_genre"]')
+        self.registration_options = ['sber', 'vk', 'google', 'yandex', 'ok']
+        self.registration_elements = [s(f'[test-id="sign_{option}"]') for option in self.registration_options]
+
     @allure.step('Open browser')
     def open(self):
         browser.open('/')
 
     @allure.step('Open registration form')
     def open_registration_form(self):
-        s('a[test-id="nav_sign"]').click()
+        self.registration_form.click()
 
     @allure.step('Check all registration options are available')
     def check_registration_options_available(self):
-        registration_options = ['sber', 'vk', 'google', 'yandex', 'ok']
-
-        for option in registration_options:
-            s(f'[test-id="sign_{option}"]').should(be.visible.and_(be.clickable))
+        for element in self.registration_elements:
+            element.should(be.visible.and_(be.clickable))
 
     @allure.step('Search film by title')
     def search_film_by_title(self, film_title):
-        s('[test-id="nav_search"]').click()
-        s('[test-id="nav_search_input"]').should(be.blank).type(film_title).press_enter()
+        self.search_button.click()
+        self.search_input_field.should(be.blank).type(film_title).press_enter()
 
     @allure.step('Check search result')
     def check_result(self, film_title):
         try:
-            browser.all('[test-id="search_collection_element"]').element_by(have.text(film_title)).should(
+            self.collection_of_elements.element_by(have.text(film_title)).should(
                 be.visible.and_(be.clickable))
         except NoSuchElementException:
             print(f'Search for the request:{film_title} did not find anything')
 
     @allure.step('Go to selected section')
     def go_to_selected_section(self, section_name):
-        ss('[test-id="rail_tab_item"]').element_by(have.exact_text(section_name)).click()
+        self.sections.element_by(have.exact_text(section_name)).click()
 
     @allure.step('Verify section name')
     def check_section_name(self, section_name):
-        s('[test-id="collection_page_title"]').should(have.exact_text(section_name))
+        self.page_title.should(have.exact_text(section_name))
 
     @allure.step('Go to selected films genre section')
     def go_to_selected_films_genre(self, genre):
-        ss('[test-id="search_collection_element"]').element_by(have.exact_text(genre)).click()
-        s('[test-id="collection_page_title"]').should(have.exact_text(genre))
+        self.collection_of_elements.element_by(have.exact_text(genre)).click()
+        self.page_title.should(have.exact_text(genre))
 
     @allure.step('Check film in section has selected genre')
     def check_first_film_has_selected_genre(self, genre):
-        ss('[test-id="search_collection_element"]').first.click()
-        s('[test-id="meta_genre"]').should(have.exact_text(genre))
+        self.collection_of_elements.first.click()
+        self.film_genre.should(have.exact_text(genre))
 
     @allure.step('Go to selected category of films')
     def go_to_selected_category(self, category):
-        s('[test-id="search_collection_element"]').s(f'//span[text()="{category}"]').click()
+        self.category.s(f'//span[text()="{category}"]').click()
 
     @allure.step('Check category title')
     def check_category_title(self, category):
-        s('[test-id="collection_page_title"]').should(have.exact_text(category))
+        self.page_title.should(have.exact_text(category))
 
 
 main_page = MainPage()
