b6d46e1b8e001f29ab94026d032f83d562695a54
Kirill Ermakov <lvstrng161@gmail.com>
2024-05-27 16:37:17 +0400

    api_call.py refactor

--- reqres_tests/request_helper/api_call.py
+++ reqres_tests/request_helper/api_call.py
@@ -4,8 +4,9 @@ from reqres_tests.utils.logger import send_request_logger
 
 class ApiCall:
 
+    @staticmethod
     @allure.step('Send API Request')
-    def send_request(self, method, base_url, payload=None):
+    def send_request(method, base_url, payload=None):
         response = send_request_logger(method=method.upper(), url=base_url, data=payload)
         return response
 
