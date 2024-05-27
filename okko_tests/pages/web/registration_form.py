319948051f9c03b38ff741ffcad01b5359ca4946
Kirill Ermakov <lvstrng161@gmail.com>
2024-05-27 09:58:46 +0400

    registration_form.py __init__ refactor

--- okko_tests/pages/web/registration_form.py
+++ okko_tests/pages/web/registration_form.py
@@ -5,18 +5,24 @@ import allure
 
 class RegistrationForm:
 
+    def __init__(self):
+        self.sign_input = s('[test-id="sign_input"]')
+        self.continue_button = s('[test-id="continue_btn"]')
+        self.sliding_panel = s('[test-id="sliding_panel"]')
+        self.info_message = s('[test-id="sign_input"]+div')
+
     @allure.step('Enter email into the field')
     def enter_email(self, email):
-        s('[test-id="sign_input"]').should(be.blank).type(email)
-        s('[test-id="continue_btn"]').click()
+        self.sign_input.should(be.blank).type(email)
+        self.continue_button.click()
 
     @allure.step('Verify registration')
     def check_registration_result(self, email_valid=True):
 
         if email_valid:
-            s('[test-id="sliding_panel"]').should(be.visible)
+            self.sliding_panel.should(be.visible)
         else:
-            s('[test-id="sign_input"]+div').should(have.exact_text('Неверный формат'))
+            self.info_message.should(have.exact_text('Неверный формат'))
 
 
 registration_form = RegistrationForm()
